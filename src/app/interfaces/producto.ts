0625a7c07aca92f7e00ecf5dc7859cc531639c80
MarAviles <83610210+MarAviles@users.noreply.github.com>
2022-07-15 22:46:19 -0500

    Add files via upload

---
+++ src/app/interfaces/producto.ts
@@ -0,0 +1,10 @@
+export interface Producto {
+    id:number,
+    nombre: string,
+    imagen: string,
+    descripcion: string,
+    stock: number,
+    precio: number,
+    categoria: string,
+    genero: string
+}
